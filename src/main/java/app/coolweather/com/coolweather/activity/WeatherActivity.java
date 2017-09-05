83f5e3b5b199e5badc2a44fb0dc53d54eff4f20b
longtuge <302673872@qq.com>
2017-09-05 11:04:17 +0800

    增加后台自动更新天气的功能。

--- src/main/java/app/coolweather/com/coolweather/activity/WeatherActivity.java
+++ src/main/java/app/coolweather/com/coolweather/activity/WeatherActivity.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import app.coolweather.com.coolweather.R;
+import app.coolweather.com.coolweather.service.AutoUpdateService;
 import app.coolweather.com.coolweather.util.HttpCallbackListener;
 import app.coolweather.com.coolweather.util.HttpUtil;
 import app.coolweather.com.coolweather.util.Utility;
@@ -180,5 +181,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText.setText(prefs.getString("current_date", ""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
---
+++ src/main/java/app/coolweather/com/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,20 @@
+package app.coolweather.com.coolweather.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import app.coolweather.com.coolweather.service.AutoUpdateService;
+
+
+/**
+ * Created by Administrator on 2017/9/5.
+ */
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, AutoUpdateService.class);
+        context.startService(i);
+    }
+}
